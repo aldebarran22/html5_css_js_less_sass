166636f1f35d795ed06400c31e41e6e29284b2ea
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-09-29 09:08:59 +0200

    viernes3

--- codigo1/js_geolocation/js/codigo.js
+++ codigo1/js_geolocation/js/codigo.js
@@ -10,6 +10,13 @@ addEventListener("load", () => {
 
         capa.innerHTML =
           "Latitud: " + lat + "<br />" + "Longitud: " + lon + "<br />";
+
+        // Mostrar un enlace con el mapa:
+        const status = document.querySelector("#status");
+        const enlace = document.querySelector("#map-link");
+
+        enlace.href = `https://openstreetmap.org/#map=18/${lat}/${lon}`;
+        enlace.textContent = `Latitud: ${lat}, Longitud: ${lon}`;
       },
       (error) => {
         // Se ha producido error
