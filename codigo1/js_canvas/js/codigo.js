ac2211fc610461ebbf969c32c8468b79d8986a3c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-09-29 09:51:03 +0200

    viernes33

--- codigo1/js_canvas/js/codigo.js
+++ codigo1/js_canvas/js/codigo.js
@@ -1,18 +1,33 @@
 let contexto;
 let colorFondo = "#FFFFFF";
-let color = "#FF0000";
-let w;
-let h;
+let color = "#000000";
+let w = 300;
+let h = 300;
 
 function pintar() {
   contexto.fillStyle = color;
-  contexto.fillRect(0, 0, h, w);
+  //contexto.fillRect(0, 0, h - 1, w - 1);
+  contexto.clearRect(0, 0, w, h);
+
+  // Nuevo trazado:
+  contexto.beginPath();
+  contexto.moveTo(100, 100);
+  contexto.lineTo(200, 200);
+  contexto.lineTo(100, 200);
+  contexto.closePath();
+
+  contexto.beginPath();
+  contexto.arc(60, 60, 50, 0, Math.PI * 2, true);
+  //contexto.closePath();
+  //contexto.moveTo(20, h - 20);
+  //contexto.lineTo(w - 20, h - 20);
+  //contexto.fill(); // Pintar con relleno
+  contexto.stroke(); // Pintar sin relleno
+  contexto.closePath();
 }
 
 addEventListener("load", () => {
   let canvas = document.getElementById("canvas");
-  w = canvas.width;
-  h = canvas.height;
   console.log("w: " + w + " h: " + h);
 
   // Recuperar el contexto del canvas
