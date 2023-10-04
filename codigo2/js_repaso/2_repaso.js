14fc86972c090fd6bc279947807f436c74d43d6f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-04 13:08:10 +0200

    mie-13-08

---
+++ codigo2/js_repaso/2_repaso.js
@@ -0,0 +1,5 @@
+import funcion, { total, sumar } from "./modulo.js";
+
+funcion();
+console.log(total);
+console.log(sumar(5, 6));
---
+++ codigo2/js_repaso/modulo.js
@@ -0,0 +1,6 @@
+const funcion = () => console.log("pruebas");
+const sumar=(a,b) => a + b;
+let total = 0;
+
+export default funcion;
+export {total,sumar};
