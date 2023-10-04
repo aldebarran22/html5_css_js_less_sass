a8334709e03674ab93965ec6b48bc20e672322b3
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-04 14:14:09 +0200

    mie-14-15

--- codigo2/dom/js/codigo.js
+++ codigo2/dom/js/codigo.js
@@ -14,7 +14,26 @@ window.onload = () => {
   boton2.addEventListener("click", () => {
     let parrafosAzul = document.querySelectorAll(".azul p");
     for (let p of parrafosAzul) {
-        p.style.color = "blue";
-      }
-    });
+      p.style.color = "blue";
+    }
+  });
+
+  let boton3 = document.querySelector("#boton3");
+  boton3.addEventListener("click", () => {
+    let capa = document.querySelector("#capa");
+    let p = document.createElement("p");
+    let texto = document.createTextNode("texto de prueba");
+    p.appendChild(texto);
+    capa.appendChild(p);
+  });
+
+  let boton4 = document.querySelector("#boton4");
+  boton4.addEventListener("click", () => {
+    let capa = document.querySelector("#capa");
+    let img = new Image();
+    img.src = "./img/foto1.jpg";
+    img.alt = "foto1.jpg";
+
+    capa.appendChild(img);
+  });
 };
