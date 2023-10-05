3bd28a2de5d90e8119843f4c4bb49730a7c107a8
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-05 09:40:25 +0200

    jue-9-40

--- codigo2/memes/js/codigo.js
+++ codigo2/memes/js/codigo.js
@@ -1,18 +1,25 @@
 const urlMemes = "https://api.imgflip.com/get_memes";
 let capa;
+const clasePorDefecto = "col-4";
 
 const procesar = (memes) => {
   console.log("num. memes: " + memes.length);
   for (let meme of memes) {
+    let capaImg = document.createElement("div");
+    capaImg.className = clasePorDefecto;
     let figure = document.createElement("figure");
     let figCaption = document.createElement("figcaption");
     let img = new Image();
     img.src = meme.url;
+    img.height = 150;
     let titulo = document.createTextNode(meme.name);
     figCaption.appendChild(titulo);
     figure.appendChild(img);
     figure.appendChild(figCaption);
-    capa.appendChild(figure);
+    capaImg.appendChild(figure);
+
+    // Añadir al contenedor principal:
+    capa.appendChild(capaImg);
   }
 };
 
