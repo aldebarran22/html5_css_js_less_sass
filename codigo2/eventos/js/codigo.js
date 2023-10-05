0e2a3edb5188437299da4474e87d95aa6312ebae
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-05 12:17:53 +0200

    jue-12-17

--- codigo2/eventos/js/codigo.js
+++ codigo2/eventos/js/codigo.js
@@ -2,19 +2,42 @@
 
 addEventListener("load", () => {
   // Al body:
-  addEventListener("click", () => {
-    console.log("click en body");
-  });
+  addEventListener(
+    "click",
+    (e) => {
+      console.log("click en body");
+    },
+    false
+  );
 
   // A la capa:
   let capa = document.getElementById("capa");
-  capa.addEventListener("click", () => {
-    console.log("click en capa");
-  });
+  capa.addEventListener(
+    "click",
+    (e) => {
+      console.log("click en capa");
+    },
+    false
+  );
 
   // Al botón:
   let boton = document.querySelector("#boton");
-  boton.addEventListener("click", () => {
-    console.log("click en botón");
-  });
+  boton.addEventListener(
+    "click",
+    (e) => {
+      console.log("click en botón");
+      // Cortar la propagación del evento:
+      //e.stopPropagation();
+    },
+    false
+  );
+
+  // Añadir eventos a los párrafos:
+  let parrafos = document.getElementsByTagName("p");
+  for (let p of parrafos) {
+    p.addEventListener("click", (e) => {
+      console.log("click en: " + e.target.textContent);
+      e.target.style.color = "red";
+    });
+  }
 });
