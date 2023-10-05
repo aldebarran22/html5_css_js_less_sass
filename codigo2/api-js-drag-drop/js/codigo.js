6b22c44575d3e2126f33c61140093a9d6e3c81b9
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-05 13:33:36 +0200

    jue-13-33

--- codigo2/api-js-drag-drop/js/codigo.js
+++ codigo2/api-js-drag-drop/js/codigo.js
@@ -1,7 +1,4 @@
-const testEventos = () => {
-  let origen = document.getElementById("origen");
-  let destino = document.getElementById("destino");
-
+const testEventos = (origen, destino) => {
   const eventosOrigen = ["dragstart", "drag", "dragend"];
   for (const nombreEvento of eventosOrigen) {
     origen.addEventListener(nombreEvento, () => {
@@ -18,5 +15,31 @@ const testEventos = () => {
 };
 
 addEventListener("load", () => {
-  testEventos();
+  let imagen = document.getElementById("image");
+  let origen = document.getElementById("origen");
+  let destino = document.getElementById("destino");
+
+  //testEventos(origen, destino);
+
+  origen.addEventListener("dragstart", (e) => {
+    let imgHTML = "<img src='" + imagen.getAttribute("src") + "'>";
+    console.log(imgHTML);
+    // Cargar el objeto en dataTransfer:
+    e.dataTransfer.setData("Text", imgHTML);
+  });
+
+  destino.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+  });
+
+  destino.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  destino.addEventListener("drop", (e) => {
+    e.preventDefault();
+    destino.innerHTML = e.dataTransfer.getData("Text");
+    // Borrar la imagen origen:
+    imagen.parentNode.removeChild(imagen);
+  });
 });
