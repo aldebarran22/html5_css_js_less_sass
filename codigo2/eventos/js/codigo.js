872c0d10d9e7828b05ffe7e6db9cc55df4ef0946
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-05 12:35:17 +0200

    jue-12-35

--- codigo2/eventos/js/codigo.js
+++ codigo2/eventos/js/codigo.js
@@ -2,35 +2,35 @@
 
 addEventListener("load", () => {
   // Al body:
-  addEventListener(
-    "click",
-    (e) => {
-      console.log("click en body");
-    },
-    false
-  );
+  addEventListener("click", (e) => {
+    console.log("click en body");
+  });
 
   // A la capa:
   let capa = document.getElementById("capa");
-  capa.addEventListener(
-    "click",
-    (e) => {
-      console.log("click en capa");
-    },
-    false
-  );
+  capa.addEventListener("click", (e) => {
+    console.log("click en capa");
+  });
+
+  capa.addEventListener("mouseover", (e) => {
+    capa.style.border = "1px solid red";
+  });
+
+  capa.addEventListener("mouseout", (e) => {
+    capa.style.border = "1px solid black";
+  });
 
   // Al botón:
   let boton = document.querySelector("#boton");
-  boton.addEventListener(
-    "click",
-    (e) => {
-      console.log("click en botón");
-      // Cortar la propagación del evento:
-      //e.stopPropagation();
-    },
-    false
-  );
+  boton.addEventListener("click", (e) => {
+    console.log("click en boton");
+    // Anular el evento
+    //e.preventDefault();
+
+    //console.log("click en botón");
+    // Cortar la propagación del evento:
+    //e.stopPropagation();
+  });
 
   // Añadir eventos a los párrafos:
   let parrafos = document.getElementsByTagName("p");
@@ -40,4 +40,14 @@ addEventListener("load", () => {
       e.target.style.color = "red";
     });
   }
+
+  // Añadir evento keypress:
+  let texto = document.querySelector("#texto");
+  texto.addEventListener("keydown", (e) => {
+    console.log("tecla: ", e.keyCode);
+    if (e.key == "Enter") {
+      console.log("enter");
+      boton.click();
+    }
+  });
 });
