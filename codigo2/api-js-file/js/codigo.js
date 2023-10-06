65401c10e9c2d2869689a17064dd0a1805cf7a5c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-06 10:42:24 +0200

    vie-9

--- codigo2/api-js-file/js/codigo.js
+++ codigo2/api-js-file/js/codigo.js
@@ -1,5 +1,29 @@
 let capa;
 
+function cargaImg(fichero) {
+  let reader = FileReader();
+  reader.addEventListener("load", (e) => {
+    // Los datos están e.target.result
+  });
+  reader.readAsDataURL(fichero);
+}
+
+function cargaTxt(fichero) {
+  let reader = FileReader();
+  reader.addEventListener("load", (e) => {
+    // Los datos están e.target.result
+  });
+  reader.readAsText(fichero);
+}
+
+function cargaBin(fichero) {
+  let reader = FileReader();
+  reader.addEventListener("load", (e) => {
+    // Los datos están e.target.result
+  });
+  reader.readAsBinaryString(fichero);
+}
+
 addEventListener("load", () => {
   capa = document.getElementById("contenedor");
   let ficheros = document.querySelectorAll("form input");
@@ -7,9 +31,23 @@ addEventListener("load", () => {
     fich.addEventListener("change", (e) => {
       let ficheros = e.target.files;
       let fichero = ficheros[0];
-      console.log("type: " + fichero.type);
+      capa.innerHTML =
+        "type: " +
+        fichero.type +
+        " name: " +
+        fichero.name +
+        " size: " +
+        fichero.size +
+        " bytes";
 
-      
+      // Filtrar el tipo de archivo:
+      if (fichero.type.match(/image.*/i)) {
+        cargaImg(fichero);
+      } else if (fichero.type == "text/plain") {
+        cargaTxt(fichero);
+      } else {
+        cargaBin(fichero);
+      }
     });
   }
 });
