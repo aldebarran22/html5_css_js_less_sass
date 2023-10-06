efaa4c4dfcdd069384e68ccc3d359541d8bedefe
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-06 09:48:53 +0200

    vie-3

--- codigo2/api-js-storage/js/codigo.js
+++ codigo2/api-js-storage/js/codigo.js
@@ -1,5 +1,30 @@
+function actualizarVista(carrito, visor) {
+  let total = 0;
+  for (const prod of carrito) {
+    total += prod.cantidad * prod.importe;
+  }
+  visor.innerHTML = `<h3>Importe: ${total} &euro;</h3>`;
+}
+
 addEventListener("load", () => {
   let carrito = [];
+  let strCarrito;
+
+  let visor = document.querySelector("#visor");
+  strCarrito = localStorage.getItem("carrito");
+  if (strCarrito == null) {
+    visor.innerHTML = "Carrito vacío";
+  } else {
+    carrito = JSON.parse(strCarrito);
+    actualizarVista(carrito, visor);
+  }
+
+  let vaciar = document.querySelector("#vaciar");
+  vaciar.addEventListener("click", () => {
+    localStorage.removeItem("carrito");
+    carrito = [];
+    visor.innerHTML = "Carrito vacío";
+  });
 
   let boton = document.querySelector("#boton");
   boton.addEventListener("click", () => {
@@ -14,6 +39,8 @@ addEventListener("load", () => {
     };
 
     carrito.push(obj);
+    actualizarVista(carrito, visor);
+
     // Grabar en LocalStorage:
     let str = JSON.stringify(carrito);
     localStorage.setItem("carrito", str);
