fda4f0934de0532d6b252f5b753feaa0447f20c1
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-06 10:15:16 +0200

    vie-6

--- codigo2/api-js-file/js/codigo.js
+++ codigo2/api-js-file/js/codigo.js
@@ -1,1 +1,11 @@
-  
+let capa;
+
+addEventListener("load", () => {
+  capa = document.getElementById("contenedor");
+  let fichero1 = document.querySelector("#fichero1");
+  fichero1.addEventListener("change", (e) => {
+    console.log("change");
+    //let ficheros = e.target.files;
+    //console.log("numFich:" + ficheros.length);
+  });
+});
