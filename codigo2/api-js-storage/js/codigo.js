3c3beabcdc61057c5090270d822c562bfa0755a8
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-06 09:27:38 +0200

    vie-2

--- codigo2/api-js-storage/js/codigo.js
+++ codigo2/api-js-storage/js/codigo.js
@@ -1,1 +1,25 @@
- 
+addEventListener("load", () => {
+  let carrito = [];
+
+  let boton = document.querySelector("#boton");
+  boton.addEventListener("click", () => {
+    let producto = document.querySelector("#producto");
+    let cantidad = document.querySelector("#cantidad");
+    let importe = document.querySelector("#importe");
+
+    obj = {
+      producto: producto.value,
+      cantidad: cantidad.value,
+      importe: importe.value,
+    };
+
+    carrito.push(obj);
+    // Grabar en LocalStorage:
+    let str = JSON.stringify(carrito);
+    localStorage.setItem("carrito", str);
+
+    producto.value = "";
+    cantidad.value = 0;
+    importe.value = 0;
+  });
+});
