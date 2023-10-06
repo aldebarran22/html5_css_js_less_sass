fe4f4eaaf6bdb746fc7c4af8992f8fa840dafdc0
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-06 12:23:05 +0200

    vie-25

--- codigo2/api-js.sockets/js/codigo.js
+++ codigo2/api-js.sockets/js/codigo.js
@@ -1,9 +1,9 @@
-let socket = null;
-
 addEventListener("load", () => {
   let bConectar = document.getElementById("conectar");
   let bDesconectar = document.getElementById("desconectar");
   let texto = document.getElementById("texto");
+  let capa = document.getElementById("contenedor");
+  let socket = null;
 
   texto.addEventListener("keypress", (e) => {
     if (e.code === "Enter") {
@@ -16,5 +16,34 @@ addEventListener("load", () => {
         alert("No estás conectado al servidor!");
       }
     }
+  });
+
+  bConectar.addEventListener("click", () => {
+    // Crear el WebSocket para conectar con el servidor:
+    socket = new WebSocket("ws://localhost:8081");
+
+    socket.addEventListener("open", () => {
+      console.log("Cliente conectado!");
+    });
+
+    // Cuando recibimos un mensaje del servidor:
+    socket.addEventListener("message", async (e) => {
+      const mensaje = await e.data;
+      capa.innerHTML += mensaje + "<br>";
+    });
+
+    socket.addEventListener("error", (e) => {
+      console.error("ERROR: " + e);
+      alert("Error: " + e);
+    });
+
+    socket.addEventListener("close", () => {
+      console.log("Desconectado  del servidor");
+    });
+  });
+
+  bDesconectar.addEventListener("click", () => {
+    console.log("Cerrar la conexión");
+    socket.close();
   });
 });
