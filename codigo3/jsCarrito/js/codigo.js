4772a020bdc7c9e1be23432dd78954a4fb649f7f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 13:08:29 +0200

    jue13_08

---
+++ codigo3/jsCarrito/js/codigo.js
@@ -0,0 +1,13 @@
+// Productos disponible:
+const carrito = [
+  { nombre: "Portatil 1", img: "../img/foto1.jpg", precio: 550 },
+  { nombre: "Portatil 2", img: "../img/foto2.jpg", precio: 650 },
+  { nombre: "Portatil 3", img: "../img/foto3.jpg", precio: 725 },
+];
+
+addEventListener("load", () => {
+
+    // Registrar eventos: drap and drop
+    
+
+});
