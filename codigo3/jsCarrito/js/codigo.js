f03f059b79a51883f7faba5b35d918849ce780da
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 13:45:09 +0200

    jue44

--- codigo3/jsCarrito/js/codigo.js
+++ codigo3/jsCarrito/js/codigo.js
@@ -1,14 +1,28 @@
 // Productos disponible:
 const productos = [
-  { nombre: "Portatil 1", img: "../img/foto1.jpg", precio: 550 },
-  { nombre: "Portatil 2", img: "../img/foto2.jpg", precio: 650 },
-  { nombre: "Portatil 3", img: "../img/foto3.jpg", precio: 725 },
+  { nombre: "Portatil 1", img: "./img/foto1.jpg", precio: 550 },
+  { nombre: "Portatil 2", img: "./img/foto2.jpg", precio: 650 },
+  { nombre: "Portatil 3", img: "./img/foto3.jpg", precio: 725 },
 ];
 
 let carrito = [];
 let capaCarrito;
+let capaAlmacen;
 
-const mostrarAlmacen = () => {};
+const mostrarAlmacen = () => {
+  for (const p of productos) {
+    let div = document.createElement("div");
+    let img = new Image();
+    img.src = p.img;
+    img.width = 75;
+    div.appendChild(img);
+    let parrafo = document.createElement("p");
+    let titulo = p.nombre + " " + p.precio + "€";
+    parrafo.appendChild(document.createTextNode(titulo));
+    div.appendChild(parrafo);
+    capaAlmacen.appendChild(div);
+  }
+};
 
 const configurarEventosDragDrop = () => {};
 
@@ -20,10 +34,11 @@ const mostrarCarrito = () => {
 };
 
 addEventListener("load", () => {
-  mostrarAlmacen();
-
   // Capturar los controles:
   capaCarrito = document.getElementById("carrito");
+  capaAlmacen = document.getElementById("almacen");
+
+  mostrarAlmacen();
 
   // Registrar eventos: drap and drop
 
