6717c1594e1f36cfb083a19e08aec69ce841ef65
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 10:45:18 +0200

    jue.88

--- codigo3/js-geo/js/codigo.js
+++ codigo3/js-geo/js/codigo.js
@@ -3,6 +3,17 @@ const mostrarInfo = (contenedor, info, color) => {
   contenedor.style.color = color;
 };
 
+const convertir = (coor, dir) => {
+  let letra, g, m;
+  if (dir == "lat") letra = coor > 0 ? "N" : "S";
+  else letra = coor > 0 ? "E" : "W";
+
+  coor = Math.abs(coor);
+  g = Math.trunc(coor);
+  m = Math.round((coor - g) * 60);
+  return `${g} ${letra} ${m}`;
+};
+
 addEventListener("load", () => {
   let contenedor = document.querySelector("#contenedor");
 
@@ -14,9 +25,12 @@ addEventListener("load", () => {
         let lat = ubicacion.coords.latitude;
         let lon = ubicacion.coords.longitude;
         let info = fecha.toString() + "<br>";
-        info += "Latitud: " + lat + "<br>";
-        info += "Longitud: " + lon + "<br>";
+        info += "Latitud: " + convertir(lat, "lat") + "<br>";
+        info += "Longitud: " + convertir(lon, "lon") + "<br>";
         mostrarInfo(contenedor, info, "blue");
+
+        const enlace = document.querySelector("#map-link");
+        enlace.href = `https://openstreetmap.org/?mlat=${lat}&mlon=${lon}#map=20/${lat}/${lon}`;
       },
       (error) => {
         mostrarInfo(contenedor, error.message, "red");
