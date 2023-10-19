07397d92bae39a36bfb967592882de1887684852
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 09:51:44 +0200

    jue7

--- codigo3/eventos/js/codigo.js
+++ codigo3/eventos/js/codigo.js
@@ -24,4 +24,20 @@ addEventListener("load", () => {
     },
     true
   );
+
+  let texto = document.getElementById("texto");
+  texto.addEventListener("keydown", (e) => {
+    console.log("e.keyCode:" + e.key);
+    if (e.key == "Enter") {
+      console.log("Pulsa enter");
+    }
+  });
+
+  let capa2 = document.getElementById("capa2");
+  capa2.addEventListener("mouseover", (e) => {
+    capa2.style.border = "2px solid red";
+  });
+  capa2.addEventListener("mouseout", (e) => {
+    capa2.style.border = "2px solid black";
+  });
 });
