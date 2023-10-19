00983b43410c2852e84ace15ae2faba6868b275e
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 13:25:05 +0200

    jue-20

--- codigo3/jsCarrito/js/codigo.js
+++ codigo3/jsCarrito/js/codigo.js
@@ -1,13 +1,32 @@
 // Productos disponible:
-const carrito = [
+const productos = [
   { nombre: "Portatil 1", img: "../img/foto1.jpg", precio: 550 },
   { nombre: "Portatil 2", img: "../img/foto2.jpg", precio: 650 },
   { nombre: "Portatil 3", img: "../img/foto3.jpg", precio: 725 },
 ];
 
+let carrito = [];
+let capaCarrito;
+
+const mostrarAlmacen = () => {};
+
+const configurarEventosDragDrop = () => {};
+
+const mostrarCarrito = () => {
+  let str = localStorage.getItem("carrito");
+  if (str == null) {
+    capaCarrito.innerHTML = "<h4>No hay productos en el carrito</h4>";
+  }
+};
+
 addEventListener("load", () => {
+  mostrarAlmacen();
 
-    // Registrar eventos: drap and drop
-    
+  // Capturar los controles:
+  capaCarrito = document.getElementById("carrito");
 
+  // Registrar eventos: drap and drop
+
+  configurarEventosDragDrop();
+  mostrarCarrito();
 });
