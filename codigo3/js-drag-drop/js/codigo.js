086df17d59e5b494e7cae449288373b53d024161
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-19 12:38:43 +0200

    jue15

--- codigo3/js-drag-drop/js/codigo.js
+++ codigo3/js-drag-drop/js/codigo.js
@@ -14,9 +14,39 @@ const testEventos = (origen, destino) => {
   }
 };
 
+const dragDrop = (origen, destino, imagen) => {
+  // Anular los eventos del destino
+  const eventosD = ["dragenter", "dragover", "drop"];
+  for (let nombre of eventosD) {
+    destino.addEventListener(nombre, (event) => {
+      // Cancela el comportamiento por defecto
+      event.preventDefault();
+    });
+  }
+
+  // dragstart en origen: cargar el texto que representa la imagen
+  // en dataTransfer
+  origen.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("Text", imagen.src);
+  });
+
+  // drop: copiar el datatransfer en el destino
+  // borrar la imagen del origen: DOM
+  destino.addEventListener("drop", (e) => {
+    let objImg = new Image();
+    objImg.src = e.dataTransfer.getData("Text");
+    destino.innerHTML = "";
+    destino.appendChild(objImg);
+    origen.removeChild(imagen);
+    // imagen.parentNode.removeChild(imagen)
+  });
+};
+
 addEventListener("load", () => {
   let origen = document.querySelector("#origen");
   let destino = document.querySelector("#destino");
+  let imagen = document.querySelector("#origen img");
 
-  testEventos(origen, destino);
+  //testEventos(origen, destino);
+  dragDrop(origen, destino, imagen);
 });
