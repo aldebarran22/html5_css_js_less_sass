ed25ecd065fe88bcd7a5325a635e1f28d86f1577
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-20 10:52:29 +0200

    vie555

--- codigo3/servidor/index.js
+++ codigo3/servidor/index.js
@@ -8,21 +8,34 @@ const server = http.createServer();
 // Crear el servidor de Web Sockets
 const wss = new WebSocket.Server({ server });
 
+// Mapa de los clientes conectados:
+mapa = new Map();
+
 // Registrar eventos:
 wss.on("connection", (socket) => {
   // El parámetro socket es el socket del cliente
   console.log("Se ha conectado un cliente: ", socket._socket.remotePort);
-  console.log("Address: ", socket._socket.address());
-  console.log("RemoteAddress: ", socket._socket.remoteAddress);
+  //console.log("Address: ", socket._socket.address());
+  //console.log("RemoteAddress: ", socket._socket.remoteAddress);
   console.log("Número de clientes conectados: ", wss.clients.size);
 
   // Comunicación con el cliente:
   socket.on("message", (data) => {
-    // Recibir mensajes del cliente
-    console.log("El cliente dice: " + data.toString());
+    let obj = JSON.parse(data.toString());
+    let port = socket._socket.remotePort;
+    mapa[port] = obj.nick;
 
     // Enviar mensajes al cliente
-    socket.send("Servidor: " + data.toString());
+    socket.send("Bienvenido: " + obj.nick);
+
+    // Presentar el nuevo usuario al resto de clientes:
+    wss.clients.forEach((cliente) => {
+      if (cliente.readyState === WebSocket.OPEN) {
+        if (cliente._socket.remotePort !== socket._socket.remotePort) {
+          cliente.send(`Se ha conectado: ${mapa[socket._socket.remotePort]}`);
+        }
+      }
+    });
   });
 
   // Un cliente se desconecta:
