2b355ca1a56dd212e1a142d46c3df19793e67c21
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-20 10:19:01 +0200

    vie555

--- codigo3/js-sockets/js/codigo.js
+++ codigo3/js-sockets/js/codigo.js
@@ -11,10 +11,40 @@ addEventListener("load", () => {
     if (e.code === "Enter") {
       // Comprobar si está conectado a un servidor
       if (socket != null && socket.readyState !== WebSocket.CLOSED) {
-        console.log("enviar un mensaje");
+        socket.send(texto.value);
       } else {
         alert("No estás conectado a un servidor");
       }
     }
+  });
+
+  // Evento boton conectar
+  bconectar.addEventListener("click", () => {
+    // Crear la conexión con el Servidor:
+    socket = new WebSocket(url);
+
+    socket.addEventListener("open", () => {
+      contenedor.innerHTML = "Conexión establecida<br>";
+    });
+
+    socket.addEventListener("message", async (e) => {
+      // Salta cuando el cliente recibe un mensaje del servidor
+      const mensaje = await e.data;
+      contenedor.innerHTML += mensaje + "<br>";
+    });
+
+    socket.addEventListener("error", (e) => {
+      contenedor.innerHTML += "ERROR: " + e.message;
+      alert("ERROR: " + e.message);
+    });
+
+    socket.addEventListener("close", () => {
+      contenedor.innerHTML += "Se ha desconectado del servidor";
+    });
+  });
+
+  // Evento boton desconectar
+  bdesconectar.addEventListener("click", () => {
+    socket.close();
   });
 });
